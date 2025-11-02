49b1845320dbf3daf12b631ba6955f2e0ea61f78
OscarOtty <138628733+OscarOtty@users.noreply.github.com>
2025-11-02 18:53:40 +0300

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,11 @@
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+// src/components/MainContent.jsx
+
+function MainContent() {
+    return (
+        <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
