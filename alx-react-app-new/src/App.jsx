936def24d4e28bd21a5c78230bfe8464f6c0035c
OscarOtty <138628733+OscarOtty@users.noreply.github.com>
2025-11-09 21:57:41 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -32,6 +33,8 @@ function App() {
         />
         
         <MainContent />
+        <Counter />
+      
         <Footer />
     </>
   );
