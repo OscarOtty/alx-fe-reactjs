59e6dbb6b82bd0dd0f24b545c18fb436ee3f6b69
OscarOtty <138628733+OscarOtty@users.noreply.github.com>
2025-11-09 21:50:09 +0300

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 function Footer() {
     return (
-        <footer>
+        <footer style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
