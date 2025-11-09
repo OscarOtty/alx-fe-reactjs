af42d955ad8b1f40f69b9753695308db415bc8d6
OscarOtty <138628733+OscarOtty@users.noreply.github.com>
2025-11-09 22:33:21 +0300

    Create ProfilePage.jsx

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import ProfilePage from './ProfilePage';
+import UserInfo from './UserInfo';
+
+function ProfilePage({ userData }) {
+  return <UserInfo userData={userData} />;
+}
+
+export default ProfilePage;
