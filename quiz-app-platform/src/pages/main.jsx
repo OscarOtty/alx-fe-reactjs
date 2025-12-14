369deb4cd63b16c2f7842ace7a461759694d0ee8
OscarOtty <138628733+OscarOtty@users.noreply.github.com>
2025-12-14 19:47:20 +0300

    Manages the caching and state of queries.

--- quiz-app-platform/src/pages/main.jsx
+++ quiz-app-platform/src/pages/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { QuizContextProvider } from '../QuizContext.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <QuizContextProvider>
+        <App />
+      </QuizContextProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 )
